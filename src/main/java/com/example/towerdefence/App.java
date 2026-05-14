3df9f2143168d643d2a6c176b938606937b164d8
william-guo08 <williamguo217@gmail.com>
2026-05-14 14:14:42 +0200

    corretto il percorso dei file

---
+++ src/main/java/com/example/towerdefence/App.java
@@ -0,0 +1,12 @@
+package com.example.towerdefence;
+
+import javafx.application.Application;
+
+public class App{
+    List<Truppa> truppe = new ArrayList<>();
+    List<Nemico> nemici = new ArrayList<>();
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/example/towerdefence/Path.java
@@ -0,0 +1,10 @@
+package com.example.towerdefence;
+
+import javafx.geometry.Point2D;
+import java.util.List;
+
+public class Path {
+    private Point2D wayIn;
+    private Point2D wayOut;
+    private List<Point2D> waypoints;
+}

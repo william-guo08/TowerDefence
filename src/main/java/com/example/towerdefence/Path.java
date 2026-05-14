ed46f8d561ee094380a352e23f44c2f070ec3876
william-guo08 <williamguo217@gmail.com>
2026-05-14 14:34:10 +0200

    aggiunto metodo per aggiungere waypoints

--- src/main/java/com/example/towerdefence/Path.java
+++ src/main/java/com/example/towerdefence/Path.java
@@ -1,10 +1,31 @@
 package com.example.towerdefence;
 
 import javafx.geometry.Point2D;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class Path {
     private Point2D wayIn;
     private Point2D wayOut;
     private List<Point2D> waypoints;
+
+    public Path(){
+        this.waypoints = new ArrayList<>();
+    }
+
+    public Path(List<Point2D> waypoints){
+        if (waypoints == null || waypoints.size() <2) {
+            throw new IllegalArgumentException("Il percorso non ha un inizio o fine");
+        }
+        this.waypoints = new ArrayList<>(waypoints);
+        this.wayIn = waypoints.get(0);
+        this.wayOut = waypoints.get(waypoints.size() -1);
+    }
+
+    public void aggiungiWaypoint(Point2D punto) {
+        waypoints.add(punto);
+        if (waypoints.size() == 1) wayIn = punto;
+        wayOut = punto;
+    }
 }

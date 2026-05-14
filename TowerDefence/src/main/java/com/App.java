fea2254f146e4845ac0e582e957b4dca75ebd507
williamGuo <williamguo217@gmail.com>
2026-05-14 14:04:48 +0200

    Aggiunto progetto con git

---
+++ TowerDefence/src/main/java/com/App.java
@@ -0,0 +1,4 @@
+public class App extends Application{
+    List<Truppa> truppe = new ArrayList<>();
+    List<Nemico> nemici = new ArrayList<>();
+}
---
+++ TowerDefence/src/main/java/com/Path.java
@@ -0,0 +1,4 @@
+public class Path {
+    private Point2D wayIn;
+    private Point2D wayOut
+}
